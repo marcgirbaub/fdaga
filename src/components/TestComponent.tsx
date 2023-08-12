27001c280c99cf4a0e3f051f5abea903f3a5011c
Gerard Ramon Monte <g.ramon@glofox.com>
2023-08-13 00:59:23 +0200

    Remove MUI theme config

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { Button } from '@mui/material';
 
 const TestComponent = () => {
-  return <div className={styles.testComponent}>a test</div>;
+  return (
+    <div className={styles.testComponent}>
+      <Button variant="contained">a test</Button>
+    </div>
+  );
 };
 
 export default TestComponent;
