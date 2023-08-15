c0cb024f6eb302d9d92dda0ececf177b42f1a13b
Gerard Ramon Monte <g.ramon@glofox.com>
2023-08-15 12:55:53 +0200

    Add gmaps map

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <TestComponent />
+
+      <iframe
+        width="600"
+        height="450"
+        loading="lazy"
+        style={{ border: 0, width: '100%' }}
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}
+          &q=Passeig+de+la+Ribera,+53,+08420+Canovelles,+Barcelona/@41.6195909,2.2832172,17z`}
+      ></iframe>
     </main>
   );
 }
