6f7cf7ce4866352e16f7599075a36656bfb2db38
Gerard Ramon Monte <g.ramon@glofox.com>
2023-08-15 12:23:01 +0200

    Add tailwind classes to material ui button

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
+import { Button } from '@mui/material';
 
 const TestComponent = () => {
   const t = useTranslations();
 
-  return <div className="text-3xl">{t('test')}</div>;
+  return (
+    <div>
+      <Button className="text-3xl">{t('test')}</Button>
+    </div>
+  );
 };
 
 export default TestComponent;
