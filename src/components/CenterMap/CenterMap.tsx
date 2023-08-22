a524f323962241794da6881fd2bc5d1428a4aa8d
Gerard Ramon Monte <g.ramon@glofox.com>
2023-08-22 09:59:18 +0200

    Fix map iframe case sensitive props

--- src/components/CenterMap/CenterMap.tsx
+++ src/components/CenterMap/CenterMap.tsx
@@ -7,8 +7,8 @@ const CenterMap: FC = () => {
         loading="lazy"
         height={200}
         style={{ border: 0, width: '100%' }}
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
         src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
           &q=Passeig+de+la+Ribera,+53,+08420+Canovelles,+Barcelona/@41.6195909,2.2832172,17z`}
       ></iframe>
