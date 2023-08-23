e3cdef8ecc659cc332f43d4822a4a7395415b0f7
Marc Girbau <marc.girbau94@gmail.com>
2023-08-23 13:16:58 +0200

    Create User model

---
+++ src/app/api/db/models/User.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+});
+
+const User = model('User', userSchema, 'users');
+
+export default User;
