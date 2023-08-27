be9e956d588ad5c41bf7af1a2d5528d83825d0a2
Gerard Ramon Monte <g.ramon@glofox.com>
2023-08-27 11:07:08 +0200

    Add gcalendar auth config and library to make requests

--- src/app/api/calendar/route.ts
+++ src/app/api/calendar/route.ts
@@ -3,13 +3,40 @@ import { NextResponse } from 'next/server';
 import { getWeekSchedule } from '../db/configModel';
 import { errorMessages } from '../errors/errors';
 import { getNext30DaysSchedule } from '../services/calendarService';
+import { google } from 'googleapis';
+import { googleCalendarCredentials } from '../lib/googleCalendar/centredagatestCredentials';
 
 export async function GET(request: Request, response: Response) {
   try {
-    const calendarEvents = await getCalendarEvents();
-    const weekSchedule = await getWeekSchedule();
+    // const calendarEvents = await getCalendarEvents();
+    // const weekSchedule = await getWeekSchedule();
 
-    getNext30DaysSchedule(calendarEvents, weekSchedule);
+    // getNext30DaysSchedule(calendarEvents, weekSchedule);
+
+    const calendarId = process.env.NEXT_PUBLIC_CALENDAR_ID;
+    const calendarClient = google.calendar({ version: 'v3' });
+    const gCalendarScopes = 'https://www.googleapis.com/auth/calendar';
+
+    const googleAuth = new google.auth.JWT(
+      googleCalendarCredentials.client_email,
+      '',
+      googleCalendarCredentials.private_key,
+      gCalendarScopes,
+    );
+
+    console.log('GETTING EVENTS........');
+
+    let eventsResponse = await calendarClient.events.list({
+      auth: googleAuth,
+      calendarId: calendarId,
+      timeMin: new Date().toISOString(),
+    });
+
+    eventsResponse.data.items?.forEach((event) => {
+      console.log(event.summary);
+    });
+
+    console.log(eventsResponse.data.items?.length);
 
     return NextResponse.json('This is working');
   } catch (error) {
@@ -20,7 +47,7 @@ export async function GET(request: Request, response: Response) {
         },
         {
           status: 500,
-        }
+        },
       );
     } else {
       return NextResponse.json(
@@ -29,7 +56,7 @@ export async function GET(request: Request, response: Response) {
         },
         {
           status: 500,
-        }
+        },
       );
     }
   }
---
+++ src/app/api/lib/googleCalendar/centredagatestCredentials.ts
@@ -0,0 +1,13 @@
+export const googleCalendarCredentials = {
+  "type": "service_account",
+  "project_id": "centredagatest",
+  "private_key_id": "c8cdb5123a90c4e5fb81ad5b32dcc281f1ff2ded",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCbJv96waArSTz4\no0CSrT+goSIyuxjzK0KaDCkUFfpS0tteLh50ZAw+r2sQX7aBDKTClZi4hEetKXy/\nBj6C5d4aR+Lb7MroHI3VNIEtP+rRtg3wAcDF4sV3lJ9o7cZxpVbXCXdpJcTS1+3H\nw9M+VYp9CyRb+Q4//+XzINndzvIP0TcCB/1sLWbh9YJ3C7uKz7/febJXiDTZhIl3\nV7QDSZE/tB0bWEn4r1Po1U3ee3b1VoNElzmrpdRRcI9AmNb5IdHWn/Se/e7IH3Qx\nFaNnRFWySqsro3gfKoaAwH8b6yATItkIQNM790CJZW1BDpoD089l/YLeunVwUtua\nWxLs46wlAgMBAAECggEASww7ktDa3WVBMVGiN/DGcLfQd3KJ2IHJ5wLP0e/plCt7\nQdyUDZ+hTqcCZjqt6FFJ1veOll4h35BMT4hBL4bp/pNcGB47bbf2ITWS8GTU/FiP\n+FKYwP92neeNji9JiIo97akeEgXbZVNeKH1ADoxI/0Am97gKXQm2XHq8964U53H+\nCF9q8wGcTt8GSXrliYbcS+iQDTerT0Jt9utvwRO5k6Ns9JjdlQK31zI5uXbVTQsT\nxUEaIgX1gnebDGApbUtej9gwj7cB0xa6xj/7i5Rp7nWZmxwX093y4TW2R1TFvIEL\npY07NYNki/PbZpKMDax8hDdWSsyg20UbB0bPgkfX5wKBgQDS+R4Z2EBYB6X1ubO5\npGcltJuUvjJwVWkP5wJzXCG2skPjfUX+ch+02YHXnhocWiOd1M0h4fAsJM3CKgk3\nsZqv6dpF/JzMOCFgnrpJiUqknUq8u/sxQDsF3+5NjFK/Ptvf1cTBBn4pFovSWwgK\njRwatAw8+ADQ1PVn9ke+MGWiIwKBgQC8RAPivhzEGe54zx3c82k+4xCSeohxE17J\nfTLflqAIhhsROi2OhpQrSPPtrPryARTTA3sR+f4lqlwWUEmh+RRKc1JMi4Vcm+ZF\nTzPHPw5p4GlJ+KAsamHqUfVOay2P3Pr+O0aiBVqU4TxmHfpL7xZ+2HChA08wfSmk\n+snTInOpFwKBgGGe3hvNYbCaEWQLgqsy9HuQ/7Cq9YHfM5clKJAioIL+J8W68MlV\nG3C+eINB6AkYeISOtECqYEy81uyyMMOxjcc6aUKbKh6hFmiz6bUfLwfOtIdyYEAg\n3W7ReF4vwRY3Stxe+QTsMTJn+VjjwGvJcai1iTgo9Y+sCsYJQpcxo8WLAoGBALf9\nAI8nnJVJgnUudNDqUYDUB+4fMhjQBWKEqKM6YUvzQ6CiasNPYp6qvTkaRGUeCWvv\niucWmfnVdAmajVtuQRZBfbyw3bST54b00HV4em2ek0dnY6IElYyabFInDG/8Vo7+\n/xh4gv3c4VorEeID1shAXlH9GfcZWwmQzo04rjAhAoGAbaQUE8OmgKhTdOqxM7Sh\nyVCiQbd7JZc28AY8+wys00alESP2t8EyWNzm39gQka/K8LudDJrMH3eX7QHEKv3A\nkm/R7IsS6+fRsfd2p2HKZ2iKzuPsScqS+b+95G9xqPvHucNfdtUOHRyrDdqlSa0e\nME2iFczP3msfAJZklCRHbyw=\n-----END PRIVATE KEY-----\n",
+  "client_email": "centredagatestcalendarnil@centredagatest.iam.gserviceaccount.com",
+  "client_id": "110208485311032047547",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/centredagatestcalendarnil%40centredagatest.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
